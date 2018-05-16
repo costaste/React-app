b525280e0c730bd70c678f9dd90699112cc5df47
Stephen Costa <st3ph3n24@gmail.com>
2018-05-16 13:17:50 -0400

    Changes for Part 3 of tutorial

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Jello World');
+/**
+ * index.js
+ */
+
+const logSomething = options => ({
+    ...options,
+    anotherOption: 'Jello!'
+});
+
+const options = logSomething({ one: '1', two: '2' });
+
+console.log(options);
