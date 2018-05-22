7d8afe32bf2b7e803433a7e40638a2b01e9d9d9f
Stephen Costa <st3ph3n24@gmail.com>
2018-05-22 12:24:09 -0400

    Added changes for Part 5 of the tutorial

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 /* GET home page */
-router.get('*', (req, res) => {
-    throw new Error('OOPS');
+router.get('/', (req, res) => {
     res.render('index');
 });
 
